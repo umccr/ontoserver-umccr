0d8db8050414a1211d7a256decf4d9a6891ff6e1
andrewpatto <andrew@patto.net>
2022-06-17 13:56:00 +1000

    Bumped release versions of ontologies

--- ontoserver-constants.ts
+++ ontoserver-constants.ts
@@ -16,10 +16,10 @@ export const STACK_DESCRIPTION =
 // Mondo  https://github.com/monarch-initiative/mondo/releases
 // SNOMED  see NCTS
 export const CURRENT_ONTOLOGIES: { [n: string]: string } = {
-  HGNC_RELEASE: "2022-01-01",
-  HPO_RELEASE: "2022-02-14",
-  HANCESTRO_RELEASE: "2.5",
-  MONDO_RELEASE: "2022-03-01",
+  HGNC_RELEASE: "2022-04-01",
+  HPO_RELEASE: "2022-06-11",
+  HANCESTRO_RELEASE: "2.6",
+  MONDO_RELEASE: "2022-06-01",
   SNOMED_RELEASE: "20220531",
 };
 
